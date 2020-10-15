44d9c8e8f59e89c11bd99ec214bd9b2dd5a8c6cc
Tamiflu233 <1714118250@qq.com>
2020-10-15 23:51:00 +0800

    完成潮流预览

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tab-control">
+    <!-- 这个组件很多地方就只是文字不一样，没必要用插槽，v-for props就可以了 -->
+    <div v-for="item in titles" :key="item">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    components:{
+
+    },
+    props: {
+      titles: {
+        type:Array,
+        default() {
+          return []
+        }
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,59 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <ul>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+      <li>哈哈</li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +63,7 @@
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper"
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
 import { getHomeMultidata } from "network/home";
 
@@ -19,7 +73,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -43,8 +98,16 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
 }
 </style>
