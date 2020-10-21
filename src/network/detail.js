bbc378f93a64ed309ff0bae1649cc52385168277
Tamiflu233 <1714118250@qq.com>
2020-10-21 10:13:16 +0800

    完成详细页的轮播图、其他信息、服务和商家信息与评分等功能

--- src/network/detail.js
+++ src/network/detail.js
@@ -1,5 +1,5 @@
 // 详细页的数据请求
-import {request} from './request';
+import { request } from './request';
 export function getDetail(iid) {
   return request({
     url: '/detail',
@@ -7,4 +7,28 @@ export function getDetail(iid) {
       iid
     }
   })
+}
+
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = services;
+    this.realPrice = itemInfo.lowNowPrice;
+  }
+}
+
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods;
+  }
 }
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,26 +1,34 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
+    <detail-nav-bar class="detail-nav-bar"></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
 
-import { getDetail } from "network/detail";
+import { getDetail, Goods, Shop } from "network/detail";
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {}
     };
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo
   },
   created() {
     // 1.保存传入的iid
@@ -29,8 +37,13 @@ export default {
     // 2.根据iid请求详细数据
     getDetail(this.iid).then(res => {
       console.log(res);
+      const data = res.result
       // 1. 获取顶部图片的轮播数据
-      this.topImages = res.result.itemInfo.topImages
+      this.topImages = data.itemInfo.topImages
+      // 2. 获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      // 3. 创建店铺信息对象
+      this.shop = new Shop(data.shopInfo)
     });
   },
   methods: {
@@ -40,4 +53,15 @@ export default {
 </script>
 
 <style scoped>
+#detail {
+  padding-top: 44px;
+}
+.detail-nav-bar {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+}
 </style>
