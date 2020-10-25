8669e232ef7584ef67df8175357b3fbef35877b3
Tamiflu233 <1714118250@qq.com>
2020-10-26 00:16:09 +0800

    完成商品评论

--- src/common/utils.js
+++ src/common/utils.js
@@ -10,4 +10,58 @@ export function debounce(func, delay) {
       func.apply(this, args);
     }, delay);
   };
+};
+/* 
+  RegExp.$1是RegExp的一个属性,指的是与正则表达式匹配的第一个 子匹配(以括号为标志)字符串，以此类推，RegExp.$2，RegExp.$3，..RegExp.$99总共可以有99个匹配
+
+
+  例子:
+
+  var r= /^(\d{4})-(\d{1,2})-(\d{1,2})$/; //正则表达式 匹配出生日期(简单匹配)     
+
+  r.exec('1985-10-15');
+
+  s1=RegExp.$1;
+
+  s2=RegExp.$2;
+
+  s3=RegExp.$3;
+
+  alert(s1+" "+s2+" "+s3)//结果为1985 10 15
+*/
+export function formatDate(date, fmt) {
+  // 1.获取年份
+  // 2019
+  // y+ -> 1个或多个y
+  // y* -> 0个或多个y
+  // y? -> 0个或1个y
+  // yy 19
+  // yyyy 2019
+  // yyy 019
+  // y 9
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  // 2.获取
+  // M+ -> 正则表达式中的一个规则
+  // M
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+
+    }
+  }
+  return fmt;
 }
+
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length);
+};
